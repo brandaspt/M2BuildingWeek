c4452163c2c7745fde3d09453950628415a9813b
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-05-11 19:50:15 +0200

    albums section finished

--- artist/script.js
+++ artist/script.js
@@ -5,57 +5,57 @@ const artistsObj = [
     albums: [
       {
         albName: "fabric presents Bonobo (DJ Mix)",
-        albCover: "../assets/artist/images/album1",
+        albCover: "../assets/artist/images/albumCovers/album1.jpeg",
         albYear: 2019,
       },
       {
         albName: "Migration",
-        albCover: "../assets/artist/images/album2",
+        albCover: "../assets/artist/images/albumCovers/album2.jpeg",
         albYear: 2017,
       },
       {
         albName: "The North Borders Tour - Live",
-        albCover: "../assets/artist/images/album3",
+        albCover: "../assets/artist/images/albumCovers/album3.jpeg",
         albYear: 2014,
       },
       {
         albName: "Late Night Tales",
-        albCover: "../assets/artist/images/album4",
+        albCover: "../assets/artist/images/albumCovers/album4.jpeg",
         albYear: 2013,
       },
       {
         albName: "The North Borders",
-        albCover: "../assets/artist/images/album5",
+        albCover: "../assets/artist/images/albumCovers/album5.jpeg",
         albYear: 2013,
       },
       {
         albName: "Black Sands Remixed",
-        albCover: "../assets/artist/images/album6",
+        albCover: "../assets/artist/images/albumCovers/album6.jpeg",
         albYear: 2012,
       },
       {
         albName: "Black Sands",
-        albCover: "../assets/artist/images/album7",
+        albCover: "../assets/artist/images/albumCovers/album7.jpeg",
         albYear: 2010,
       },
       {
         albName: "Days To Come",
-        albCover: "../assets/artist/images/album8",
+        albCover: "../assets/artist/images/albumCovers/album8.jpeg",
         albYear: 2006,
       },
       {
         albName: "Dial 'M' for Monkey",
-        albCover: "../assets/artist/images/album9",
+        albCover: "../assets/artist/images/albumCovers/album9.jpeg",
         albYear: 2003,
       },
       {
         albName: "One Offs (Remixes $ B Sides)",
-        albCover: "../assets/artist/images/album10",
+        albCover: "../assets/artist/images/albumCovers/album10.jpeg",
         albYear: 2002,
       },
       {
         albName: "Animal Magic",
-        albCover: "../assets/artist/images/album11",
+        albCover: "../assets/artist/images/albumCovers/album11.jpeg",
         albYear: 2000,
       },
     ],
@@ -73,6 +73,7 @@ const heroContent = document.querySelector("main .hero-content");
 
 window.onload = () => {
   populateHeroContent();
+  populateAlbums();
 };
 
 const populateHeroContent = () => {
@@ -88,6 +89,40 @@ const populateHeroContent = () => {
         <i class="fas fa-ellipsis-h"></i>
     </div>
   `;
+};
+
+const populateAlbums = () => {
+  const albumsGrid = document.querySelector("#albums > .albums-cards");
+  for (const album of artistsObj[0].albums) {
+    albumsGrid.innerHTML += `
+        <div class="col p-0">
+                <div class="card border-0 p-2 mx-1 h-100">
+                  <img
+                    src="${album.albCover}"
+                    class="card-img-top"
+                    alt="${album.albName}"
+                  />
+                  <div class="card-body text-center p-1 d-flex flex-column justify-content-between">
+                    <p class="card-title fw-bold">${album.albName}</p>
+                    <p class="card-text fw-bold">${artistsObj[0].name}</p>
+                  </div>
+                  <button class="btn rounded-circle card-play-btn">
+                    <svg
+                      height="16"
+                      role="img"
+                      width="16"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true">
+                      <polygon
+                        points="21.57 12 5.98 3 5.98 21 21.57 12"
+                        fill="currentColor"
+                      ></polygon>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+        `;
+  }
 };
 
 // Change active link on main nav
